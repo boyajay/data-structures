93be71669fe5c75e49df3c879945d1a1c34d56d3
boyajay <boyajay@github.com>
2016-01-13 15:34:30 -0800

    implement queue function for functioncal shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
+  var someInstance = {};
+  someInstance.queueStart = 1;
+  someInstance.queueEnd = 0;
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  return someInstance;
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(val){
+    this.queueEnd++;
+    this[this.queueEnd] = val;
+  },
+  dequeue: function(){
+    var result;
+    if(this.queueEnd + 1 > this.queueStart){
+      result = this[this.queueStart];
+      this.queueStart++;
+    }
+    if(result){
+      return result;
+    }
+  },
+  size: function(){
+    return this.queueEnd - this.queueStart + 1;
+  }
+};
+
 
 

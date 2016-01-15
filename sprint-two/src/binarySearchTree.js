bdf6f356c0a723ecd84d9176b50e186d46f504cc
boyajay <boyajay@github.com>
2016-01-15 12:32:47 -0800

    implemented binary tree insert function & contain function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,49 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(BinarySearchTree.prototype);
+  newTree.value = value || null;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
 
+BinarySearchTree.prototype.insert= function(n) {
+  if (this && this.value){
+    if (n> this.value){
+      if (!this.right){
+        this.right = BinarySearchTree(n);
+      } else {
+        this.insert.call(this.right, n);
+      }
+    } else if (n < this.value) {
+        if (!this.left){
+        this.left = BinarySearchTree(n);
+      } else {
+        this.insert.call(this.left, n);
+      }
+    }
+  } else{
+    if (this){ 
+      this.value = n;
+    }
+  }
+};
 
+BinarySearchTree.prototype.contains = function(n) {
+  if(this.value === n){
+    return true;
+  } else if(n > this.value && this.right){
+    return this.contains.call(this.right, n);
+  } else if(n < this.value && this.left){
+    return this.contains.call(this.left, n);
+  } else {
+    return false;
+  }
+  
+  // body...
+};
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */

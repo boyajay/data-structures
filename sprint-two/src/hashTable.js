b3e789db4984dddcae7e5b288fe290fb48ae160b
boyajay <boyajay@github.com>
2016-01-14 21:27:54 -0800

    Implement hash table constructor object

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,25 +2,41 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this.indexes = {};           //holds keys
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this.indexes.hasOwnProperty(index) && (k !== this.indexes[index])){
+    for(var i = index + 1; i < this._limit; i++){
+      if (!this._storage.get(i)){
+      this.indexes[i] = k;
+      this._storage.set(i, v);
+      break;
+      }
+    }
+  } else {
+    this.indexes[index] = k;
+    this._storage.set(index, v);
+    console.log(this.indexes);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (k === this.indexes[index]){
+    return this._storage.get(index);
+  } else {
+      for(var i = index + 1; i < this._limit; i++){
+        if (this.indexes[i] === k){
+            return this._storage.get(i);
+          }
+      }
+  } 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-

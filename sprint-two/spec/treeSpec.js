4c079e00381e67c7e339d298a28aed520afddc7a
boyajay <boyajay@github.com>
2016-01-16 11:41:19 -0800

    Correct specs for removeFromParent

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -47,7 +47,8 @@ describe('tree', function() {
     tree.children[0].addChild(7);
     tree.children[1].addChild(8);
     tree.children[0].removeFromParent();
-    tree.children[1].removeFromParent();
+    tree.children[0].removeFromParent();
+    
     expect(tree.contains(5)).to.equal(false);
     expect(tree.contains(7)).to.equal(false);
     expect(tree.contains(8)).to.equal(false);

c0438756f18623b211a73e9e26a0d149e79fa28f
boyajay <boyajay@github.com>
2016-01-16 11:41:41 -0800

    Implement removeFromParent function for Tree Constructor

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,12 +1,13 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.parent = null;
 
   // your code here
   newTree.children = null;  // fix me
   newTree.addChild = treeMethods.addChild;
   newTree.contains = treeMethods.contains;
-
+  newTree.removeFromParent =treeMethods.removeFromParent;
   return newTree;
 };
 
@@ -15,6 +16,7 @@ var treeMethods = {};
 treeMethods.addChild = function(value) {
   this.children = this.children || [];
   this.children.push(Tree(value));
+  this.children[this.children.length-1].parent = this;
 };
 
 treeMethods.contains = function(target) {
@@ -31,7 +33,14 @@ treeMethods.contains = function(target) {
   return false;
 };
 
-
+treeMethods.removeFromParent = function() {
+  //console.log(this.parent.children.indexOf(this));
+  var removalIndex = this.parent.children.indexOf(this);
+  childArray = this.parent.children;
+  console.log(Array.isArray(childArray));
+  childArray.splice(removalIndex, 1);
+  this.parent = null;
+};
 
 
 

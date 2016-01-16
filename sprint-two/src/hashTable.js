cbb2b7e6dc6e7db35e4c96b0cccb64ec27befa24
boyajay <boyajay@github.com>
2016-01-15 22:15:49 -0800

    Add incomplete hashTable changeSize function

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,6 +2,7 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._size = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -10,22 +11,69 @@ var HashTable = function() {
   //   checkLimit(index);
   //   storage.get(index) = value;
   // };
+HashTable.prototype.changeSize = function(which){
+    var listOfTuples= [];
+    console.log("which is " + which + ", and limit is " + this._limit);
+    for (var i =0 ; i < this._limit; i++){ //iterates through Hash indexes
+        if (!(this._storage.get(i))){
+        //console.log('skip');
+        continue;
+      } else {
+        var arr = this._storage.get(i);
+        for(var b = 0; b <arr.length; b++){ //looping through an index's nested array
+          listOfTuples.push([arr[b][0], arr[b][1]]);  
+        }
+      }
+    }
+
+    if(which === 'double') {  
+    //console.log("new limit should be" + (this._limit*2));
+    this._limit *= 2;
+    this._storage = LimitedArray(this._limit);
+    //this._limit *= 2;
+   // console.log(this._limit);
+    for (var j = 0; j < listOfTuples.length; j++){
+      this.insert(listOfTuples[j][0], listOfTuples[j][1]);
+    }
+  } 
+
+  else {
+
+//    console.log('old ' + this._limit );
+    this._limit /= 2;
+    this._storage = LimitedArray(this._limit);
+  //  console.log('old ' + this._limit );
+   // console.log('new ' + this._limit );
+    for (var j = 0; j < listOfTuples.length; j++){
+      this.insert(listOfTuples[j][0], listOfTuples[j][1]);
+    }  
+  }
+};
 
 
 HashTable.prototype.insert = function(k, v) {
+  console.log('insterting');
+  if (this._size + 1 > (0.75 * this._limit)){
+  //  console.log('old ' + this._limit);
+    console.log("about to double");
+    this.changeSize('double');
+    //console.log('new ' + this._limit);
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) === undefined){
     var newIndexVal = [[k, v]];
     this._storage.set(index, newIndexVal);
+    ////console.log(this.retrieve(k));
+    this._size++; 
   }
    else {
       var hasFound = false;
       var bucket = this._storage.get(index);
         for(var i = 0; i < bucket.length; i++){
-        console.log("entered finding loop");
         if (bucket[i][0] === k){
           bucket[i][1] = v;
           this._storage.set(index, bucket);
+          this._size++; 
           //this._storage.get(index)[i][1] = v;
           hasFound = true;
           break;
@@ -34,6 +82,7 @@ HashTable.prototype.insert = function(k, v) {
       if(!hasFound){
         var subArrayLength = this._storage.get(index).length;
         this._storage.get(index)[this._storage.get(index).length] = [k, v];
+        this._size++; 
       }
   }
 };
@@ -52,11 +101,17 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  console.log("size and limit are " + this._size + ", " + this._limit);
+  if ((this._size - 1) < (0.25 * this._limit)){
+      this.changeSize('half');
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) && this._storage.get(index).length > 1){
     for(var i = 0; i < this._storage.get(index).length; i++){
       if (this._storage.get(index)[i][0] === k){
-        this._storage.get(index)[i][1] = undefined; 
+        var bucket = this._storage.get(index)[i];
+        delete bucket; //i[0] === key
+        this._size--;
         break;      
       } 
     }
